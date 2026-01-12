89e2cf84be1c153080d78a70bd03bdd1b4cf593d
momomo0206 <momomo0206@gmail.com>
2026-01-12 21:22:55 +0900

    feat: App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import { AuthProvider } from './context/AuthContext';
+import { ToastProvider } from './context/ToastContext';
+import SignupPage from './pages/SignupPage';
+import RoomsPage from './pages/RoomPage';
+import Protected from './components/Protected';
+import AboutPage from './pages/AboutPage';
+import { ToastContainer } from './components/Toast';
+import ChatPage from './pages/ChatPage';
+import ProfilePage from './pages/ProfilePage';
+import PrivacyPage from './pages/PrivacyPage';
+import TermsPage from './pages/TermsPage';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ToastProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/signup' element={<SignupPage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/privacy' element={<PrivacyPage />} />
+            <Route path='/terms' element={<TermsPage />} />
 
-export default App
+            <Route element={<Protected />}>
+              <Route path='/rooms' element={<RoomsPage />} />
+              <Route path='/chat/:roomId' element={<ChatPage />} />
+              <Route path='/profile' element={<ProfilePage />} />
+              <Route index element={<Navigate to='/rooms' replace />} />
+            </Route>
+
+            <Route path='*' element={<Navigate to='/login' replace />} />
+          </Routes>
+          <ToastContainer />
+        </BrowserRouter>
+      </AuthProvider>
+    </ToastProvider>
+  );
+}
